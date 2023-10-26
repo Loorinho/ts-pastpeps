b75e94f3ef9e78a87d66841c293523a8d6d96698
Loorinho <romeojacobson@gmail.com>
2023-10-26 09:11:25 +0300

    Added login and registration files

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import { Route, Routes } from "react-router-dom"
 import './index.css'
 import Layout from "./features/layout/Layout"
 import Home from "./features/home/Home"
+import Login from "./features/login/Login"
 
 function App() {
  
   return (
    <Routes>
-    <Route path="/" element={<h1>Home</h1>} />
+    <Route path="/" element={<Login />} />
     <Route path="/home" element={<Layout />}>
       <Route index element={<Home />} />
     </Route>
